67eafb823764bc721f72075a29da6b1fccec4444
stephbatliner <stephanie.batliner@digital.justice.gov.uk>
2024-09-25 16:55:10 +0100

    v2 Case list and updated jason files

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,45 @@
-let tasks = require('./tasks.json')
+let mytasks = require('./mytasks.json')
 
 module.exports = {
-  tasks
+  mytasks
+}
+
+
+
+let yourtasks = require('./yourtasks.json')
+
+module.exports = {
+  yourtasks
+}
+
+
+
+let postponedcases = require('./postponedcases.json')
+
+module.exports = {
+  postponedcases
+}
+
+
+
+let readyforrelease = require('./readyforrelease.json')
+
+module.exports = {
+  readyforrelease
+}
+
+
+
+let releasedonhdc = require('./releasedonhdc.json')
+
+module.exports = {
+  releasedonhdc
+}
+
+
+
+let inactivecases = require('./inactivecases.json')
+
+module.exports = {
+  inactivecases
 }
