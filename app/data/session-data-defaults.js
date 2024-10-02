efc909e192ef840dcfad95a5e79f1580be1423fd
stephbatliner <stephanie.batliner@digital.justice.gov.uk>
2024-10-02 10:20:35 +0100

    v2 Updated case list

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,21 +1,15 @@
-let mytasks = require('./mytasks.json')
-
-module.exports = {
-  mytasks
-}
-
-
-
-let yourtasks = require('./yourtasks.json')
-
-module.exports = {
-  yourtasks
-}
-
-
-
 let inactivecases = require('./inactivecases.json')
 
 module.exports = {
   inactivecases
 }
+
+
+let ppdmcases = require('./ppdmcases.json')
+
+module.exports = {
+  ppdmcases
+}
+
+
+
